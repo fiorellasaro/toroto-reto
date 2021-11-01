4eb53123ebe87a8e77f917b78b143e6414007749
fiorellasaro <fiorellasaro27@gmail.com>
2021-10-31 20:09:56 -0500

    mobile queries style index

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
 import Head from 'next/head';
 import Image from 'next/image'
+import Link from 'next/link'
+import ArrowIcon from '../assets/chevron-down.svg'
 
 import { Container, Row, Col } from 'react-bootstrap'
 
 import Map from '../components/map'
 import CardProject from '../components/cardProject'
 import {getIndex} from '../shared/_imageProjects';
+import {useRef} from 'react'
 
 
  
 const Home = ({projectsData}) => {
+
+  const myRef = useRef(null)
+
+  const executeScroll = () => myRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });  
 
   return (
 
@@ -19,7 +26,7 @@ const Home = ({projectsData}) => {
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
-    <Container className="container-fluid">
+    <Container className="container-fluid main-container">
 
       <Row>
         <Col className="banner-content col-md-4">
@@ -27,20 +34,26 @@ const Home = ({projectsData}) => {
               Somos desarrolladores de proyectos
           </h1>
           <p className="lead">
-            Lorem ipsum
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
           </p>
 
           <a className="btn btn-secondary btn-lg" href="/">
             Proponer un proyecto
           </a>
+
+            <div onClick={executeScroll} className="button-see-more-projects-container">
+              <p>VER LISTA COMPLETA DE PROYECTOS</p>
+              <Image src={ArrowIcon} alt="arrow down to see projects"></Image>
+            </div>
+
         </Col>
       </Row>
     </Container>
-    <section fluid='true'>
+    <section fluid='true' className="map-container">
       <Map projectsData= {projectsData} />
     </section>
-    <section className="projects-cards-container">
-          <h1 className="projects-cards-section-title">
+    <section  className="projects-cards-container">
+          <h1 ref={myRef} className="projects-cards-section-title">
               Todos nuestros proyectos
 
           </h1>
